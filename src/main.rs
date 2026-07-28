9d7b0349cf50cd54364d8947a1b45fae7b82ce13
Oleg Bogdanov <boguscoder@gmail.com>
2026-07-28 10:44:21 -0700

    got normal mode working

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 
 mod usb;
 
-use bmp388_embedded::{Address, Oversampling, r#async::Bmp388Async};
+use bmp388_embedded::{
+    Address, IirFilter, OutputDataRate, Oversampling, PowerMode, SensorConfig, r#async::Bmp388Async,
+};
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
-use embassy_rp::i2c::{self, Config, InterruptHandler as I2CInterruptHandler};
+use embassy_rp::i2c::{self, InterruptHandler as I2CInterruptHandler};
 use embassy_rp::peripherals::I2C1;
 use embassy_time::{Delay, Duration, Ticker};
 use panic_probe as _;
@@ -25,7 +27,9 @@ async fn main(spawner: Spawner) {
     let scl = p.PIN_3;
 
     log::info!("set up i2c ");
-    let i2c = i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, Config::default());
+    let mut i2c_config = i2c::Config::default();
+    i2c_config.frequency = 400_000;
+    let i2c = i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, i2c_config);
 
     let baro_result = Bmp388Async::new(i2c, Delay, Address::Secondary).await;
 
@@ -33,20 +37,35 @@ async fn main(spawner: Spawner) {
         panic!("Failed to initialize Barometer")
     };
 
-    baro.set_oversampling(Oversampling::X8, Oversampling::X2)
+    baro.set_sensor_config(SensorConfig {
+        pressure_oversampling: Oversampling::X8,
+        temperature_oversampling: Oversampling::X1,
+        iir_filter: IirFilter::Off,
+        output_data_rate: OutputDataRate::Hz50,
+    })
+    .await
+    .ok();
+
+    embassy_time::Timer::after_millis(10).await;
+
+    baro.set_power_control(true, true, PowerMode::Normal)
         .await
         .unwrap();
 
-    let mut imu_ticker = Ticker::every(Duration::from_hz(1000));
+    embassy_time::Timer::after_millis(10).await;
+
+    let pwr = baro.power_control().await.unwrap();
+    log::info!("Barometer Power Status: {:?}", pwr.mode);
+    let mut imu_ticker = Ticker::every(Duration::from_hz(50));
 
     log::info!("Pressure(Pa), Temperature(C)");
     loop {
-        let Ok(measurement) = baro.forced_measurement().await else {
+        let Ok(measurement) = baro.sensor_data().await else {
             continue;
         };
 
+        log::info!("Barometer Power Status: {:?}", pwr.mode);
         log::info!("{}, {}", measurement.pressure, measurement.temperature);
-        // Delay until next loop
         imu_ticker.next().await;
     }
 }
